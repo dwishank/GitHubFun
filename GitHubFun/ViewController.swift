88069631b8ebb3ec4d2a1c8f503dd3e9187c8398
Dwishank Patil <dwishankk@dwishanks-macbook-pro.local>
2018-08-25 14:00:59 +0530

    Initial Commit

---
+++ GitHubFun/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitHubFun
+//
+//  Created by Dwishank Patil on 25/08/18.
+//  Copyright © 2018 Dwishank Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
